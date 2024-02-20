95d7f99a712f2af9519ceef957086ad39f2f25d4
Staffan Lindstrom <fanstaf@hotmail.com>
2024-02-20 10:45:02 -0500

    fix lualine, lua-ls, lualine

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,9 +1,9 @@
 return {
     {
-    "williamboman/mason.nvim",
-    config = function()
-        require("mason").setup()
-    end
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
@@ -17,22 +17,36 @@ return {
                     "cssls",
                     "jsonls",
                     "marksman",
-                    "sqlls"
-                }
+                    "sqlls",
+                },
             })
-        end
+        end,
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
+            lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = {
+                        format = {
+                            enable = true,
+                            -- Put format options here
+                            -- NOTE: the value should be STRING!!
+                            defaultConfig = {
+                                indent_style = "space",
+                                indent_size = "4",
+                            },
+                        },
+                    },
+                },
+            })
             lspconfig.phpactor.setup({
                 on_attach = on_attach,
                 init_options = {
                     ["language_server_phpstan.enabled"] = false,
                     ["language_server_psalm.enabled"] = false,
-                }
+                },
             })
             lspconfig.tsserver.setup({})
 
@@ -41,7 +55,6 @@ return {
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-
-        end
-    }
+        end,
+    },
 }
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,58 @@
 return {
     "nvim-lualine/lualine.nvim",
     config = function()
-        require('lualine').setup {
+        local separator = { '"▏"' }
+        require("lualine").setup({
             options = {
-                theme = 'dracula'
-            }
-        }
-    end
+                section_separators = "",
+                component_separators = "",
+                globalstatus = true,
+                theme = {
+                    normal = {
+                        a = "StatusLine",
+                        b = "StatusLine",
+                        c = "StatusLine",
+                    },
+                },
+            },
+            sections = {
+                lualine_a = {
+                    "mode",
+                    separator,
+                },
+                lualine_b = {
+                    "branch",
+                    "diff",
+                    separator,
+                    '"🖧  " .. tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
+                    { "diagnostics", sources = { "nvim_diagnostic" } },
+                    separator,
+                },
+                lualine_c = {},
+                lualine_x = {
+                    {
+                        "filename",
+                        path = 1,
+                    },
+                    "filetype",
+                    "encoding",
+                    "fileformat",
+                },
+                lualine_y = {
+                    separator,
+                    '(vim.bo.expandtab and "␠ " or "⇥ ") .. " " .. vim.bo.shiftwidth',
+                    separator,
+                },
+                lualine_z = {
+                    "location",
+                    "progress",
+                },
+            },
+        })
+        -- require('lualine').setup {
+        --     options = {
+        --         theme = 'dracula'
+        --     }
+        -- }
+    end,
 }
