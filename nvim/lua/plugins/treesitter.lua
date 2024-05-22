72656b91b920b94b1a6cc696894c5cebefbdadf4
Staffan Lindstrom <fanstaf@hotmail.com>
2024-05-22 15:21:49 -0500

    added nvim-ts-autotag + changed bottom scrolloff

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
     build = ":TSUpdate",
+    dependencies = { "windwp/nvim-ts-autotag" },
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
