d2c4e992c344a838263add520f9ca5c5668fee06
Staffan Lindstrom <fanstaf@hotmail.com>
2024-05-22 15:06:48 -0500

    Added which-key and more descriptions for keybindings

--- nvim/lua/plugins/git-integration.lua
+++ nvim/lua/plugins/git-integration.lua
@@ -1,14 +1,16 @@
 return {
     {
         "lewis6991/gitsigns.nvim",
+        event = { "BufReadPre", "BufNewFile" },
         config = function()
             require("gitsigns").setup()
             vim.keymap.set("n", "]h", ":Gitsigns next_hunk<CR>")
             vim.keymap.set("n", "[h", ":Gitsigns prev_hunk<CR>")
-            vim.keymap.set("n", "gs", ":Gitsigns stage_hunk<CR>")
-            vim.keymap.set("n", "gS", ":Gitsigns undo_stage_hunk<CR>")
-            vim.keymap.set("n", "gp", ":Gitsigns preview_hunk<CR>")
-            vim.keymap.set("n", "gb", ":Gitsigns blame_line<CR>")
+            vim.keymap.set("n", "gs", ":Gitsigns stage_hunk<CR>", { desc = "Stage hunk" })
+            vim.keymap.set("n", "gS", ":Gitsigns undo_stage_hunk<CR>", { desc = "Undo stage hunk"})
+            vim.keymap.set('n', "gr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk" })
+            vim.keymap.set("n", "gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview hunk"})
+            vim.keymap.set("n", "gb", ":Gitsigns blame_line<CR>", { desc = "Blame line"})
         end,
     },
     {
