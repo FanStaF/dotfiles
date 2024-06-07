1ef7e7369c829dbeee52435213fb116db3802641
Staffan Lindstrom <fanstaf@hotmail.com>
2024-06-07 07:36:47 -0500

    fixed formatting (+maybe some lsp fixes)

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,94 +1,94 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"phpactor",
-					"tsserver",
-					"html",
-					"cssls",
-					"tailwindcss",
-					"stimulus_ls",
-					"jsonls",
-					"marksman",
-					"sqlls",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "phpactor",
+                    "tsserver",
+                    "html",
+                    "cssls",
+                    "tailwindcss",
+                    "stimulus_ls",
+                    "jsonls",
+                    "marksman",
+                    "sqlls",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
         event = { "BufReadPre", "BufNewFile" },
         dependencies = { "antosha417/nvim-lsp-file-operations", config = true },
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				settings = {
-					Lua = {
-						format = {
-							enable = true,
-							-- Put format options here
-							-- NOTE: the value should be STRING!!
-							defaultConfig = {
-								indent_style = "space",
-								indent_size = "4",
-							},
-						},
-					},
-				},
-			})
-			lspconfig.phpactor.setup({
-				on_attach = on_attach,
-				init_options = {
-					["language_server_phpstan.enabled"] = false,
-					["language_server_psalm.enabled"] = false,
-				},
-			})
-            lspconfig.stimulus_ls.setup({
-                capabilities = capabilities
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = {
+                        format = {
+                            enable = true,
+                            -- Put format options here
+                            -- NOTE: the value should be STRING!!
+                            defaultConfig = {
+                                indent_style = "space",
+                                indent_size = "4",
+                            },
+                        },
+                    },
+                },
             })
-			lspconfig.tsserver.setup({
-				root_dir = function(...)
-					return require("lspconfig.util").root_pattern(".git")(...)
-				end,
-				single_file_support = false,
-				settings = {
-					typescript = {
-						inlayHints = {
-							includeInlayParameterNameHints = "literal",
-							includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-							includeInlayFunctionParameterTypeHints = true,
-							includeInlayVariableTypeHints = false,
-							includeInlayPropertyDeclarationTypeHints = true,
-							includeInlayFunctionLikeReturnTypeHints = true,
-							includeInlayEnumMemberValueHints = true,
-						},
-					},
-					javascript = {
-						inlayHints = {
-							includeInlayParameterNameHints = "all",
-							includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-							includeInlayFunctionParameterTypeHints = true,
-							includeInlayVariableTypeHints = true,
-							includeInlayPropertyDeclarationTypeHints = true,
-							includeInlayFunctionLikeReturnTypeHints = true,
-							includeInlayEnumMemberValueHints = true,
-						},
-					},
-				},
-			})
-			lspconfig.html.setup({
+            lspconfig.phpactor.setup({
+                on_attach = on_attach,
+                init_options = {
+                    ["language_server_phpstan.enabled"] = false,
+                    ["language_server_psalm.enabled"] = false,
+                },
+            })
+            lspconfig.stimulus_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.tsserver.setup({
+                root_dir = function(...)
+                    return require("lspconfig.util").root_pattern(".git")(...)
+                end,
+                single_file_support = false,
+                settings = {
+                    typescript = {
+                        inlayHints = {
+                            includeInlayParameterNameHints = "literal",
+                            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                            includeInlayFunctionParameterTypeHints = true,
+                            includeInlayVariableTypeHints = false,
+                            includeInlayPropertyDeclarationTypeHints = true,
+                            includeInlayFunctionLikeReturnTypeHints = true,
+                            includeInlayEnumMemberValueHints = true,
+                        },
+                    },
+                    javascript = {
+                        inlayHints = {
+                            includeInlayParameterNameHints = "all",
+                            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                            includeInlayFunctionParameterTypeHints = true,
+                            includeInlayVariableTypeHints = true,
+                            includeInlayPropertyDeclarationTypeHints = true,
+                            includeInlayFunctionLikeReturnTypeHints = true,
+                            includeInlayEnumMemberValueHints = true,
+                        },
+                    },
+                },
+            })
+            lspconfig.html.setup({
                 capabilities = capabilities,
                 init_options = {
-                    configurationSection = { "html", "css", "javascript" },
+                    configurationSection = { "html", "css", "javascript", "blade" },
                     embeddedLanguages = {
                         css = true,
                         javascript = true,
@@ -97,20 +97,32 @@ return {
                 },
             })
             lspconfig.cssls.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
-			lspconfig.tailwindcss.setup({
-                capabilities = capabilities
+            lspconfig.tailwindcss.setup({
+                cmd = { "tailwindcss-language-server", "--stdio" },
+                filetypes = {
+                    "html",
+                    "css",
+                    "scss",
+                    "javascript",
+                    "javascriptreact",
+                    "typescript",
+                    "typescriptreact",
+                    "vue",
+                },
+                root_dir = lspconfig.util.root_pattern("tailwind.config.js", "package.json"),
+                settings = {},
             })
 
-			vim.keymap.set("n", "<leader>pm", ":PhpactorContextMenu<CR>")
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Goto definition" })
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Goto references" })
-			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Goto Implementation"})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions"})
-			vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
-		end,
-	},
+            vim.keymap.set("n", "<leader>pm", ":PhpactorContextMenu<CR>")
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Goto definition" })
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Goto references" })
+            vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Goto Implementation" })
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
+            vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
+            vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
+        end,
+    },
 }
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,58 +1,58 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-        local separator = { '"▏"' }
-        require("lualine").setup({
-            options = {
-                section_separators = "",
-                component_separators = "",
-                globalstatus = true,
-                theme = {
-                    normal = {
-                        a = "StatusLine",
-                        b = "StatusLine",
-                        c = "StatusLine",
-                    },
-                },
-            },
-            sections = {
-                lualine_a = {
-                    "mode",
-                    separator,
-                },
-                lualine_b = {
-                    "branch",
-                    "diff",
-                    separator,
-                    '"🖧  " .. tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
-                    { "diagnostics", sources = { "nvim_diagnostic" } },
-                    separator,
-                },
-                lualine_c = {},
-                lualine_x = {
-                    {
-                        "filename",
-                        path = 1,
-                    },
-                    "filetype",
-                    "encoding",
-                    "fileformat",
-                },
-                lualine_y = {
-                    separator,
-                    '(vim.bo.expandtab and "␠ " or "⇥ ") .. " " .. vim.bo.shiftwidth',
-                    separator,
-                },
-                lualine_z = {
-                    "location",
-                    "progress",
-                },
-            },
-        })
-        -- require('lualine').setup {
-        --     options = {
-        --         theme = 'dracula'
-        --     }
-        -- }
-    end,
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		local separator = { '"▏"' }
+		require("lualine").setup({
+			options = {
+				section_separators = "",
+				component_separators = "",
+				globalstatus = true,
+				theme = {
+					normal = {
+						a = "StatusLine",
+						b = "StatusLine",
+						c = "StatusLine",
+					},
+				},
+			},
+			sections = {
+				lualine_a = {
+					"mode",
+					separator,
+				},
+				lualine_b = {
+					"branch",
+					"diff",
+					separator,
+					'"🖧  " .. tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
+					{ "diagnostics", sources = { "nvim_diagnostic" } },
+					separator,
+				},
+				lualine_c = {},
+				lualine_x = {
+					{
+						"filename",
+						path = 1,
+					},
+					"filetype",
+					"encoding",
+					"fileformat",
+				},
+				lualine_y = {
+					separator,
+					'(vim.bo.expandtab and "␠ " or "⇥ ") .. " " .. vim.bo.shiftwidth',
+					separator,
+				},
+				lualine_z = {
+					"location",
+					"progress",
+				},
+			},
+		})
+		-- require('lualine').setup {
+		--     options = {
+		--         theme = 'dracula'
+		--     }
+		-- }
+	end,
 }
